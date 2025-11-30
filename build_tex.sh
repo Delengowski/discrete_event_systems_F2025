ee9e3d43d97e297e87ac7af8fa9888dfccfcaf55
Matt Delengowski <matt.delengowski@gmail.com>
2025-11-29 19:43:58 -0500

    Started skeleton for paper and latex build script

--- build_tex.sh
+++ build_tex.sh
@@ -6,3 +6,4 @@ docker build -t report_builder .
 docker create --name built_report report_builder
 docker cp built_report:/app/main.log ./build_artifacts/
 docker cp built_report:/app/main.pdf ./build_artifacts/
+docker rm built_report
